7a2200f8e542fcacb1fe51d1ebff1a58f645efa3
stefanfredik <stefanfredik@gmail.com>
2022-11-29 18:51:03 +0800

    fix Controller : User, Datasiswa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,16 +4,19 @@ namespace App\Controllers;
 
 use App\Models\PesertaModel;
 use App\Models\SiswaModel;
+use Myth\Auth\Models\UserModel;
 
 class Home extends BaseController {
     public function __construct() {
         $this->siswaModel = new SiswaModel();
         $this->pesertaModel = new PesertaModel();
+        $this->userModel = new UserModel();
     }
+
     public function index() {
         $data = [
             'title' => 'Halaman Utama',
-            'jumlahUser' => $this->siswaModel->countAll(),
+            'jumlahUser' => $this->userModel->countAll(),
             'jumlahSiswa' => $this->siswaModel->countAll(),
             'jumlahPeserta' => $this->pesertaModel->countAll(),
             'url'   => [
